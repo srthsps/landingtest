6e4900e20258a367bf900725247a49dc117efbf1
sarathsps <srthsps22@gmail.com>
2021-08-09 21:44:49 +0530

    SECOND commit

--- js/main.js
+++ js/main.js
@@ -5,13 +5,12 @@ function toggleMenu() {
     if (menu.classList.contains("active")) {
         menu.classList.remove("active");
 
-        // add menu icon
         toggle.innerHTML = `<i class="fas fa-bars">`;
     }
     else {
         menu.classList.add("active");
 
-        // add x (close menu) icon
+
         toggle.innerHTML = `<i class="fas fa-times"></i>`;
     }
 }
